927fa1925b246ebb95a0f384cf325e29d8aef51e
JKimani77 <jkkimani77@gmail.com>
2021-04-17 12:06:43 +0300

    Created an articles model and initialised it

--- app/models.py
+++ app/models.py
@@ -1,9 +1,26 @@
 class Newssourcemodel:
 
     def __init__(self,id,title,overview,news_image,person_posted,time_posted)
-    self.id = id
-    self.title = title
-    self.overview = overview
-    self.news_image = "https://image.tmdb.org/t/p/w500/" + news_image
-    self.person_posted = person_posted
-    self.time_posted = time_posted
+        self.id = id
+        self.title = title
+        self.overview = overview
+        self.news_image = "https://image.tmdb.org/t/p/w500/" + news_image
+        self.person_posted = person_posted
+        self.time_posted = time_posted
+
+
+class Newsarticlemodel:
+
+    all_articles = []
+
+    def __init__(self,article_id, title, article_image,article_description )
+        self.article_id = article_id
+        self.title = title
+        self.article_image = article_image
+        self.article_description = article_description
+
+
+    #classmethod
+    #function for getting articles from arr
+    #return
+
