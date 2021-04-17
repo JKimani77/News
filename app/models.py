825460d832464ccdd119e422992f8f4f8475bb56
JKimani77 <jkkimani77@gmail.com>
2021-04-17 11:59:59 +0300

    Created a News Model and initialised it

---
+++ app/models.py
@@ -0,0 +1,9 @@
+class Newssourcemodel:
+
+    def __init__(self,id,title,overview,news_image,person_posted,time_posted)
+    self.id = id
+    self.title = title
+    self.overview = overview
+    self.news_image = "https://image.tmdb.org/t/p/w500/" + news_image
+    self.person_posted = person_posted
+    self.time_posted = time_posted
---
+++ start.ps1
@@ -0,0 +1,4 @@
+$env:NEWS_API_KEY='c7da1eee3a5f79d86b3e341754a6bb13'
+$env:SECRET_KEY='moringa1234'
+
+python manage.py server
